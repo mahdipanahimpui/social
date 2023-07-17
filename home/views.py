6662bf63483204dcae5a4ecb6d86be8fb9e3d8f2
mahdi <mahdipanahimpui@gmail.com>
2023-07-17 02:13:19 -0400

    class_base_View

--- home/views.py
+++ home/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 
-# Create your views here.
-def home(request):
-    return render(request, 'home/index.html') # point to templates > home > index.html
+
+class HomeView(View):
+    
+    def get(self, request): # if user come with GET method
+        return render(request, 'home/index.html') # point to templates > home > index.html
+        
+    def post(self, request):
+        pass
