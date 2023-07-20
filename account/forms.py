2994454efabe9f76740924caa643ff3aa1d96139
mahdi <mahdipanahimpui@gmail.com>
2023-07-20 07:14:10 -0400

    user_login

--- account/forms.py
+++ account/forms.py
@@ -50,3 +50,9 @@ class UserRegisterForm(forms.Form):
         if p1 and p1 and p1 != p2:
             # validation on over form not specific fields
             raise ValidationError('passwords must match')
+
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    
