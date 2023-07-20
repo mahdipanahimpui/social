ea345a510d189c6feb5d49b8703799974d18550e
mahdi <mahdipanahimpui@gmail.com>
2023-07-20 05:05:41 -0400

    8_clean-specific-form-field(validation)

--- account/forms.py
+++ account/forms.py
@@ -1,7 +1,37 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class UserRegisterForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
+
+
+    # field validation, prevent from same username or email
+    # run when is_valid() is called
+    # def clean_<filed_name>:
+    
+    def clean_email(self):
+        # the same clean_data in form
+        email = self.cleaned_data['email']
+        # exists() just return true or false not all data,(is optimized)
+        user = User.objects.filter(email=email).exists()
+
+        if user:
+            raise ValidationError('the emial already exists')
+        # is required to return email
+        return email
+    
+    
+    def clean_username(self):
+    # the same clean_data in form
+        username = self.cleaned_data['username']
+        # exists() just return true or false not all data,(is optimized)
+        user = User.objects.filter(username=username).exists()
+
+        if user:
+            raise ValidationError('the username already exists')
+        # is required to return email
+        return username
