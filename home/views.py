0527413aa8d17f4f15c9e17deb71afc2327bf54f
mahdi <mahdipanahimpui@gmail.com>
2023-07-21 12:06:18 -0400

    post_update_1

--- home/views.py
+++ home/views.py
@@ -4,6 +4,7 @@ from django.views import View
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
+from .forms import PostUpdateForm
 
 
 class HomeView(View):
@@ -33,3 +34,31 @@ class PostDeleteView(LoginRequiredMixin ,View):
             messages.error(request, 'cant delete this post', 'warning')
         
         return redirect('account:user_profile', user_id=request.user.id)
+    
+
+
+
+
+class PostUpdateView(LoginRequiredMixin, View):
+    form_class = PostUpdateForm
+    template_name = 'home/update.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        post = Post.objects.get(pk=kwargs['post_id'])
+        print(post.user.id)
+        print(request.user.id)
+
+        if post.user.id != request.user.id:
+            messages.error(request, 'you cant Edit this post', 'danger')
+            return redirect('account:user_profile', user_id=request.user.id)
+        # retruning super().dispatch() is very important
+        # it used as else
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, post_id):
+        post = Post.objects.get(pk=post_id)
+        form = self.form_class(instance=post)
+        return render(request, self.template_name, {'form':form})
+    
+    def post(self, request, post_id):
+        pass
