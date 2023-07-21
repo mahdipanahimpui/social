92b9ffffcafdb43069721259361323c6bc78d0de
mahdi <mahdipanahimpui@gmail.com>
2023-07-21 07:42:27 -0400

    customizing_admin_pannel_for_a_model

--- home/admin.py
+++ home/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 
 from .models import Post
 
+# @admin.register(Post) second way
+class PostAdmin(admin.ModelAdmin):
+    # to show fields in admin panel
+    list_display = ('user', 'slug', 'updated')
+    search_fields = ('slug', 'body')
+    list_filter = ('updated', 'created')
+    # to pre_complete slug field
+    prepopulated_fields = {'slug': ('body',)}
+    # insted drop down menu for author(user), use user id
+    raw_id_fields = ('user',)
 
-admin.site.register(Post)
+
+
+
+admin.site.register(Post, PostAdmin) # second way to show PostAdmin
