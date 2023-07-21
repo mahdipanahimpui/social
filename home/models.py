13fd269d43de1d85695db9215104c837998bd01a
mahdi <mahdipanahimpui@gmail.com>
2023-07-21 07:16:09 -0400

    Post_model_(admin_panel,foreinkey)

--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # a foreinKey to user model, if user deleted, all post shuld be deleted
+    body = models.TextField()
+    slug = models.SlugField()
+    # auto_now_add, complete the input automaticaly for FIRST time
+    created = models.DateField(auto_now_add=True) #
+    # auto_now update the Datetime field after every save
+    updated = models.DateField(auto_now=True) # 
+
+
+    def __str__(self):
+        return f'{self.slug} - by: {self.user}'
