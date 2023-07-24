0a42acc1b72509be8ecf8ad24c2d3f7cd5b21b7d
mahdi <mahdipanahimpui@gmail.com>
2023-07-24 06:55:30 -0400

    extend_user_model

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
-from .models import Relation
+from .models import Relation, Profile
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False # delete btn in admin pannel is disabled
+
+
+class ExtendedUserAdmin(UserAdmin):
+    # add profile inline to extended user admin
+    inlines = (ProfileInline, )
+
+# unregister the User model
+admin.site.unregister(User)
+# register the extended user model
+admin.site.register(User, ExtendedUserAdmin)
 
 
 admin.site.register(Relation)
--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-
 from .models import Post, Comment, Like
 
 @admin.register(Post) # first way
