5d9ddd6c0d3e5570108f57f0a00ed3fb5f7be382
mahdi <mahdipanahimpui@gmail.com>
2023-07-24 06:40:34 -0400

    change_migrated_databse

--- account/models.py
+++ account/models.py
@@ -7,5 +7,12 @@ class Relation(models.Model):
     to_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
     created = models.DateTimeField(auto_now_add=True)
 
+    #### adding new field after migration ####
+    # ! ERROR for previous data because the dont have new fields:
+    # sulotion:
+    # 1_ Delete Databse and create again
+    # 2_ put default value =>     default='empty'
+    # 3_ put null value => null = True, blank = True
+
     def __str__(self):
         return f'{self.from_user} TO {self.to_user}'
